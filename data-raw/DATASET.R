c05955e5be18efb6dfac95a50f4356879be9b55a
Sarah Popov <sarah.popov@gmail.com>
2023-03-22 18:50:19 -0700

    Update sample and location data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,12 @@
+# Locations data
 locations <- read.csv("data-raw/locations.csv")
 locations$site <- factor(locations$site)
 
 usethis::use_data(locations, overwrite = TRUE)
+
+# Samples data
+samples <- read.csv("data-raw/samples.csv")
+samples$date_collected <- as.Date(samples$date_collected)
+samples$rain_yn <- ifelse(samples$rain_yn == 1, TRUE, FALSE)
+
+usethis::use_data(samples, overwrite = TRUE)
